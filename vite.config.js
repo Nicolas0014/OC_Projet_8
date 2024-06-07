81ae195470783d524ff09da3ee9c378e3865f391
Nicolas Charbonnier <charbonnier.nicolas@outlook.fr>
2024-06-07 08:14:37 +0200

    base name vite

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/OC_Projet_8',
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
